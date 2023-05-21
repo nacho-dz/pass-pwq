1b91047188c5cdfd95c178adf4cd2816986d2ef4
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-05-21 11:41:57 -0300

    use $COMMAND in help

--- src/pwq.bash
+++ src/pwq.bash
@@ -23,25 +23,25 @@ cmd_pwq_version() {
 cmd_pwq_usage() {
 	cat <<-_EOF
 	Usage:
-	    $PROGRAM pwq generate [--clip,-c] [--in-place,-i | --force,-f] pass-name [entropy-bits]
+	    $PROGRAM $COMMAND generate [--clip,-c] [--in-place,-i | --force,-f] pass-name [entropy-bits]
 	        Generate a new password of entropy-bits bits of entropy (or $GENERATED_STRENGTH if unspecified).
 	        Optionally put it on the clipboard and clear board after $CLIP_TIME seconds.
 	        Prompt before overwriting existing password unless forced.
 	        Optionally replace only the first line of an existing file with a new password.
 	        More information may be found in the pwmake(1) man page.
-	    $PROGRAM pwq score [-u, --user=user] pass-name
+	    $PROGRAM $COMMAND score [-u, --user=user] pass-name
 	        Check the quality of a password.
 	        If provided, check similarity of the password to the username
 	        (use the password file's basename if unspecified).
 	        More information may be found in the pwscore(1) man page.
-	    $PROGRAM pwq find-weak [-u, --user=user] [-m, --min-score=min-score] [subfolder]
+	    $PROGRAM $COMMAND find-weak [-u, --user=user] [-m, --min-score=min-score] [subfolder]
 	        Find passwords in this password storage which score lower than min-score
 	        (or $MINIMUM_SCORE if unspecified).
 	        The optional user argument is identical to that of the score sub-command.
 	        Optionally only search the given subdirectory.
-	    $PROGRAM pwq help
+	    $PROGRAM $COMMAND help
 	        Show this text.
-	    $PROGRAM pwq version
+	    $PROGRAM $COMMAND version
 	        Show version information.
 
 	More information may be found in the pass-pwq(1) man page.
